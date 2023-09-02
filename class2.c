a42f0eb2f531169cda82b68a7e6daddab20c0b2f
Evan <md.mofazzalhossain18@gmail.com>
2023-09-02 23:30:44 +0600

    c

---
+++ class2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+
+int age = 25;
+
+int firstNumber = 33;
+printf("\n firstNumber =%d", firstNumber);
+int secondNumber = firstNumber;
+printf("\n secondNumber = %d", secondNumber);
+
+printf("\n Age: %d", age);
+
+age = 31;
+
+printf("\n New Age: %d", age);
+
+
+
+return 0;
+}
