5aaf54e1ba33b7ea86bbae871cd646369f8451af
Evan <md.mofazzalhossain18@gmail.com>
2023-09-11 21:38:02 +0600

    update

---
+++ scanfForInput.c
@@ -0,0 +1,28 @@
+#include <stdio.h> 
+
+int main() {
+//     int age;
+
+// printf("Enter input\n");
+// scanf("\n %d", &age);
+
+
+
+// printf("\nAge = %d", age);
+
+double number;
+char alphabet;
+
+printf("\n Enter double number");
+scanf("%lf", &number);
+
+printf("\n Enter Char");
+scanf("\n%c", &alphabet);
+
+printf("\n Number: %lf", number);
+printf("\n Char: %C ", alphabet);
+
+
+    return 0;
+
+}
