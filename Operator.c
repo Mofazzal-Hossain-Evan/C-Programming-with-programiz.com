0a3bf38dd1684f28e4510c8723cf67e747148f64
Evan <md.mofazzalhossain18@gmail.com>
2023-09-12 20:36:09 +0600

    multiple operator

--- Operator.c
+++ Operator.c
@@ -1,16 +1,24 @@
 #include<stdio.h>
 
 int main() {
+
+
     // double x = 12.34;
     // int y = 8;
-
     // double result = x + y;
-
     // printf("%lf\n",result);
 
-    int x = 12;
-    int result = x % 8;
-    printf("Result: %d\n ", result);
+    // int x = 12;
+    // int result = x % 8;
+    // printf("Result: %d\n ", result);
+
+    //int x = 12;
+
+    //printf("%d", ++x);
+    //printf("%d", --x);
+
+int x = (4/ 2) + (6 * 5)- 1;
+    printf("%d", x);
 
     return 0;
 }
