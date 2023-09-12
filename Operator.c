d39efcf5baa1c21a4d2227b954c7849f89bb818b
Evan <md.mofazzalhossain18@gmail.com>
2023-09-12 20:18:36 +0600

    int devided by 8

--- Operator.c
+++ Operator.c
@@ -1,10 +1,16 @@
 #include<stdio.h>
 
 int main() {
-    double x = 12.34;
-    int y = 8;
+    // double x = 12.34;
+    // int y = 8;
 
-    double result = x + y;
+    // double result = x + y;
 
-    printf("%lf\n",result);
+    // printf("%lf\n",result);
+
+    int x = 12;
+    int result = x/8;
+    printf("Result: %d\n ", result);
+
+    return 0;
 }
