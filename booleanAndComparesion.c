d69625c20de4893c897963c8dd8718f65d29ccca
Evan <md.mofazzalhossain18@gmail.com>
2023-09-14 23:02:55 +0600

    boolean

---
+++ booleanAndComparesion.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int main() {
+    bool value = true;
+    bool value2 = false;
+    
+    printf("%d\n", value);
+    printf("%d\n", value2);
+    return 0;
+}
