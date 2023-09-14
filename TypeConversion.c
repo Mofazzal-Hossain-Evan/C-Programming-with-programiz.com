0f78d4465500bd8777a4a4997aa991443e5a5341
Evan <md.mofazzalhossain18@gmail.com>
2023-09-14 22:05:45 +0600

    inside Conversion

---
+++ TypeConversion.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() {
+   // char a = 'a';
+
+   double a = 5.67;
+    int b = 9;
+
+    //int result = a + b;
+    int result = (int)a + b;
+
+    printf("%d" , result);
+
+    return 0;
+
+}
