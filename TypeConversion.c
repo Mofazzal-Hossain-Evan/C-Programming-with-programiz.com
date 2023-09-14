03f430c5049f41d3805519791869db56c44cd584
Evan <md.mofazzalhossain18@gmail.com>
2023-09-14 22:11:51 +0600

    double and int conversion

--- TypeConversion.c
+++ TypeConversion.c
@@ -3,13 +3,22 @@
 int main() {
    // char a = 'a';
 
-   double a = 5.67;
-    int b = 9;
+//    double a = 5.67;
+//     int b = 9;
 
     //int result = a + b;
-    int result = (int)a + b;
+   //double result = a + b;
+    // int result = (int)a + b;
 
-    printf("%d" , result);
+    // printf("%d" , result);
+
+
+    int a = 9;
+    int b = 2;
+
+    double result = (double)a / b;
+
+    printf("%lf", result);
 
     return 0;
 
