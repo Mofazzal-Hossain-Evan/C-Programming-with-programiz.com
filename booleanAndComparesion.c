59772c46b8659f78509f6f76a9f735a35466e378
Evan <md.mofazzalhossain18@gmail.com>
2023-09-18 22:01:33 +0600

    and operator

--- booleanAndComparesion.c
+++ booleanAndComparesion.c
@@ -2,10 +2,19 @@
 #include<stdbool.h>
 
 int main() {
-    bool value = true;
-    bool value2 = false;
-    
-    printf("%d\n", value);
-    printf("%d\n", value2);
+    //bool value = true;
+   // bool value2 = false;
+
+   //bool value = (5 > 6);
+   //bool value = (9 == 6);
+   //bool value = ( 4 != 5);
+   //bool value = (5 >= 6);
+
+   int age = 18;
+   double height = 6.3;
+  
+  bool result = (age >= 18) && (height > 6.0);  
+    printf("%d\n", result);
+    //printf("%d\n", value2);
     return 0;
 }
