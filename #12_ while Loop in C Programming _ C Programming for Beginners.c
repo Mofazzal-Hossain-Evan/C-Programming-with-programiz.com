41628ccc6162764cfcaa758919a3b8b572cbf2a9
Evan <119051240+Mofazzal-Hossain-Evan@users.noreply.github.com>
2023-12-25 11:33:51 +0600

    complete

--- #12_ while Loop in C Programming _ C Programming for Beginners.c
+++ #12_ while Loop in C Programming _ C Programming for Beginners.c
@@ -2,13 +2,37 @@
 
 int main() {
 
-    int count = 1;
-    while(count < 5) {
+    // int count = 1;
+    // while(count < 5) {
         
-        printf("count = %d\n", count);
-        count = count + 1;
+    //     printf("count = %d\n", count);
+    //     count = count + 1;
 
-    }
+    // } 
+
+
+
+
+
+
+    // int number ;
+    // printf("Enter the number: "); 
+    // scanf("%d", &number);
+     
+    // int count = 1;
+    // while (count <= 10) {
+    //     int product = number*count;
+    //     printf("%d*%d = %d\n", number, count, product);
+    //     count = count + 1;
+    // }
+
+
+int count = 5;
+
+do {
+   printf("%d\n", count);
+   count = count + 1;
+} while(count < 5);
 
     return 0;
 }
