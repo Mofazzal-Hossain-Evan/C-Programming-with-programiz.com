1b4d23944037ad6e6775cebcd6ca09a265383e04
Evan <119051240+Mofazzal-Hossain-Evan@users.noreply.github.com>
2023-12-26 12:14:19 +0600

    complete

---
+++ #17 C Standard Library Functions _ C Programming For Beginners.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include <math.h>
+#include<ctype.h>
+
+int main() {
+
+
+
+    //mathheader
+    // int num = 25;
+    // printf("square root %lf", sqrt(num));
+
+    
+
+    // int num = 25;
+    // printf("cube root root %lf", cbrt(num));
+
+
+    // int num = 25;
+    // int num2 = 55;
+    // double result = pow(num, num2);
+    // printf("power %lf", result);
+
+
+
+
+    // char alpha = 'e';
+    // char upper = toupper(alpha);
+    // printf("%c", upper);
+
+    // char lower = tolower(upper);
+    // printf("\n%c", lower);
+
+
+    
+
+
+
+
+}
