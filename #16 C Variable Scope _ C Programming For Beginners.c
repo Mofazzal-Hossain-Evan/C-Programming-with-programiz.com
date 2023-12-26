1370b75cbba5285f0ba382760961fbd237fd391f
Evan <119051240+Mofazzal-Hossain-Evan@users.noreply.github.com>
2023-12-26 10:13:15 +0600

    complete

---
+++ #16 C Variable Scope _ C Programming For Beginners.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int result;
+void addNumber(int number1, int number2) {
+    int result = number1 + number2;
+    return result;
+}
+
+int main() {
+
+ // int sum = addNumber(5,6);
+    printf("result = %d", result);
+}
