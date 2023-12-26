e506afa8df2af696735b36015147fbf256be3773
Evan <119051240+Mofazzal-Hossain-Evan@users.noreply.github.com>
2023-12-27 01:22:32 +0600

    complete

---
+++ #19 C Arrays _ C Programming For Beginners.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    //int age[5] = {21, 29, 25, 32,17};
+
+    // int age[5];
+    // age[0] = 12;
+
+    // printf("%d", age[0]);
+
+    // printf("Enter 5 input: ");
+
+    // scanf("%d", &age[1]);
+
+    // printf("%d", age[1]);
+
+
+
+
+
+// int age[5];
+// for(int i =0; i < 5; i++) {
+//     scanf("%d \t", &age[i]);
+// }
+
+// for(int i =0; i<5; i++) {
+//     printf("%d ", age[i]);
+// }
+
+
+
+
+
+
+
+}
