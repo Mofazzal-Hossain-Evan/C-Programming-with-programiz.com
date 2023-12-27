623462d98f35f0f1655eb8f43a164c1f7fc075df
Evan <119051240+Mofazzal-Hossain-Evan@users.noreply.github.com>
2023-12-27 18:40:25 +0600

    complete

---
+++ #22 C String Functions _ C Programming For Beginners.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<string.h>
+
+
+int main() {
+
+// char language[] = "c pro";
+
+// printf("%s", language);
+
+// printf("\nLength: %zu", strlen(language));
+
+
+
+// char foods[] ="pizza";
+// char bestfood[strlen(foods)];
+
+// strcpy(bestfood, foods);
+
+// printf("%s", bestfood);
+
+
+
+
+char text1[] = "abcd";
+char text2[] = "abcd";
+
+int result = strcmp(text1,text2);
+printf("%d", result);
+
+
+
+}
