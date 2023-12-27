ca5ab2b5e8a5ea3b4200948679367736da122f17
Evan <119051240+Mofazzal-Hossain-Evan@users.noreply.github.com>
2023-12-27 19:58:55 +0600

    commit

---
+++ #23 C Pointers _ C Programming For Beginners.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() {
+    int age = 25;
+    // printf("%p", &age);
+
+
+  //  int* ptr = &age;
+   // int var;
+
+   int* ptr = 31;
+   printf("%p", &age);
+
+    //printf("Adderss : %p",ptr);
+    //printf("value: %d \n", *ptr);
+}
